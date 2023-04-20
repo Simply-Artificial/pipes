11319cac16b59f6d38a5e912ee880963f7302f35
alf1e <alfiewbarlow@gmail.com>
2023-04-20 17:48:07 +0100

    Add: project boilerplate

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='sa-pipes',
+    version='{{VERSION_PLACEHOLDER}}',
+    description="Simply Artificial's transformer pipelines",
+    url='https://github.com/Simply-Artificial/pipes',
+    author='ItsMeAlfie0',
+    author_email='simply-artificial@itsmealfie0.com',
+    license='MIT',
+    packages=[
+        'pipes'
+    ],
+    install_requires=open('requirements.txt').read().splitlines(),
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
