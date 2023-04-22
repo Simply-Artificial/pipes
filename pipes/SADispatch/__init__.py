989dca5bf663d38ad07a7e278cbcc7c048eb41a4
alf1e <alfiewbarlow@gmail.com>
2023-04-22 19:54:07 +0100

    Add __init__.py to SADispatch

---
+++ pipes/SADispatch/__init__.py
@@ -0,0 +1,5 @@
+from pipes.SADispatch.SADispatchPathing import SADispatchPathway, SADispatchPathingCollection
+from pipes.SADispatch.SADispatchKeywordDetection import SADispatchKeywordDetection
+from pipes.SADispatch.SADispatchQuestionDetection import SADispatchQuestionDetection
+from pipes.SADispatch.SADispatchSynonymFetcher import get_synonyms
+from pipes.SADispatch.SADispatchPipeline import SADispatchPipeline
