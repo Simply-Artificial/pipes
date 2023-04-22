3a20039299bad3cb78c7d879a1b7589418166035
alf1e <alfiewbarlow@gmail.com>
2023-04-22 19:53:53 +0100

    Update: BetterConversations.py

--- pipes/BetterConversations.py
+++ pipes/BetterConversations.py
@@ -1,17 +1,12 @@
 from transformers import Conversation
 
+__all__ = ["BetterConversation"]
+
 
 class BetterConversation(Conversation):
-    def to_dict(self):
+    def to_dict(self) -> dict:
         return {
-            "uuid": self.uuid,
+            "conversation_id": self.uuid,
             "past_user_inputs": self.past_user_inputs,
             "generated_responses": self.generated_responses,
-            "new_user_input": self.new_user_input,
         }
-
-    def from_dict(self, d: dict):
-        self.uuid = d["uuid"]
-        self.past_user_inputs = d["past_user_inputs"]
-        self.generated_responses = d["generated_responses"]
-        self.new_user_input = d["new_user_input"]
